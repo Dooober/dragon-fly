457494518b428d8cad639214a2e7531218126f7d
Dooober <cooperdean2002@gmail.com>
2023-02-24 15:01:57 -0500

    Boundary fix + gitignore update

--- Dragon Fly!/game.cpp
+++ Dragon Fly!/game.cpp
@@ -1,7 +1,10 @@
 #include "GameManager.h"
+#include "WorldManager.h"
+#include "LogManager.h"
+#include "DisplayManager.h"
 
 int main() {
 	GM.startUp();
-
+	LM.writeLog("%d", WM.getBoundary().getHorizontal());
 	GM.shutDown();
 }
